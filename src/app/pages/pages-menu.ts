50d5035fee48211c5e2a69d65b11a3b6f318b122
skchevuru <44297978+skchevuru@users.noreply.github.com>
2020-01-09 09:55:52 -0500

    NGX Basic Structure
    NGX Basic structure

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,10 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'E-commerce',
+    icon: 'shopping-cart-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+];
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { ECommerceComponent } from './e-commerce/e-commerce.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PagesComponent,
+  children: [
+    {
+      path: 'dashboard',
+      component: ECommerceComponent,
+    },
+    {
+      path: '',
+      redirectTo: 'dashboard',
+      pathMatch: 'full',
+    },
+  ],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
